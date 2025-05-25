c1b504c3b0f2fd994343732c00189cdbf841c5b4
M-KE24 <mikeangelosumido1993@gmail.com>
2025-05-26 00:59:57 +0800

    Update scrt-nba.js

--- scrt-nba.js
+++ scrt-nba.js
@@ -49,7 +49,7 @@ document.addEventListener("DOMContentLoaded", async () => {
         key: '0125600d0eb13359c28bdab4a2ebe75a',
                  },	 
          {        		 
-        name: 'NBA- GAME 2-Indiana Pacers vs New York Knicks 8:00am (May 24, 2025)',
+        name: 'NBA- GAME 3-New York Knicks vs Indiana Pacers 8:00am (May 26, 2025)',
         logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxKvWOlRRjQqfWsCLfQB-Or4BQcA8juneenZJDBv5t6cBFXhqEvucmeVE&s=10',
         type: 'mpd',
         url: 'https://qp-pldt-live-grp-02-prod.akamaized.net/out/u/pl_nba.mpd',
